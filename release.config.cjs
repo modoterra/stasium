02c16afbad67004818e46e7bf22be4c3e51c45a5
Christoffer Hallas <git@christofferhallas.com>
2026-03-02 21:45:28 -0500

    fix: add gpg signing to release workflow and restore changelog plugin

--- release.config.cjs
+++ release.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     ["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     [
       "@semantic-release/github",
       {
